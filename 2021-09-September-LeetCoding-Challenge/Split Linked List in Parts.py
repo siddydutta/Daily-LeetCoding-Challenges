2726a1024077b62f3b28902b294ffcc627df5fe0
Siddhartha Dutta <siddharthadutta@protonmail.com>
2021-09-29 22:03:49 +0530

    September 29

--- 2021-09-September-LeetCoding-Challenge/Split Linked List in Parts.py
+++ 2021-09-September-LeetCoding-Challenge/Split Linked List in Parts.py
@@ -10,21 +10,67 @@ class ListNode:
 
 
 class Solution:
-    def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
-        return [head]
+    def splitListToParts(self, head: Optional[ListNode],
+                         k: int) -> List[Optional[ListNode]]:
+        # Find linkedlist length
+        node = head
+        length = 0
+        while node:
+            node = node.next
+            length += 1
+
+        # Compute number of elements per part and extra parts required
+        n_elements = length // k
+        extra_parts = length % k
+
+        node = head
+        result = list()
+        for _ in range(k):
+            if node is None:
+                result.append(None)
+                continue
+            new_head = None
+            ptr = new_head
+            # Create new linkedlist with elements for part
+            for i in range(n_elements):
+                new_node = ListNode()
+                new_node.val = node.val
+                if ptr is None:
+                    new_head = new_node
+                    ptr = new_node
+                else:
+                    ptr.next = new_node
+                    ptr = ptr.next
+                node = node.next
+
+            # Add extra element for first n%k parts
+            if extra_parts > 0:
+                new_node = ListNode()
+                new_node.val = node.val
+                if ptr is None:
+                    new_head = new_node
+                    ptr = new_node
+                else:
+                    ptr.next = new_node
+                node = node.next
+                extra_parts -= 1
+
+            result.append(new_head)
+
+        return result
 
 
 def main():
     head = ListNode(1)
     head.next = ListNode(2)
-    head.next = ListNode(3)
+    head.next.next = ListNode(3)
     k = 5
     result = Solution().splitListToParts(head, k)
-    assert result[0] == ListNode(1)
-    assert result[1] == ListNode(2)
-    assert result[2] == ListNode(3)
-    assert result[3] == []
-    assert result[4] == []
+    assert result[0].val == 1
+    assert result[1].val == 2
+    assert result[2].val == 3
+    assert not result[3]
+    assert not result[4]
 
 
 if __name__ == '__main__':
