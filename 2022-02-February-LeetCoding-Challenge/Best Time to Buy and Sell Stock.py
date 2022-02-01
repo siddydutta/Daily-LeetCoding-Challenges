624706a3de9747d78353283b348fe17d936b8e4b
Siddhartha Dutta <siddharthadutta@protonmail.com>
2022-02-01 08:49:20 +0530

    Feb 1

---
+++ 2022-02-February-LeetCoding-Challenge/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        ''' Track minimum price so far and update profit accordingly. '''
+        min_price, max_profit = prices[0], 0
+        for price in prices[1:]:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price-min_price)
+        return max_profit
+
+
+def main():
+    prices = [7, 1, 5, 3, 6, 4]
+    assert Solution().maxProfit(prices) == 5
+
+    prices = [7, 6, 4, 3, 1]
+    assert Solution().maxProfit(prices) == 0
+
+
+if __name__ == '__main__':
+    main()
