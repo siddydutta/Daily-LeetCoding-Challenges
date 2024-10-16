bfb19ef5e276d966ad65dec210472918ed44534a
Siddhartha Dutta <siddharthadutta@protonmail.com>
2024-10-16 11:13:10 +0100

    Oct 16

--- 2024-10-October-LeetCoding-Challenge/Longest Happy String.py
+++ 2024-10-October-LeetCoding-Challenge/Longest Happy String.py
@@ -4,15 +4,15 @@ class Solution:
         n_a, n_b, n_c = 0, 0, 0
         for _ in range(a + b + c):
             if (a >= b and a >= c and n_a < 2) or \
-                (n_b == 2 and a > 0 and n_a < 2) or \
-                    (n_c == 2 and a > 0 and n_a < 2):
+               (n_b == 2 and a > 0 and n_a < 2) or \
+               (n_c == 2 and a > 0 and n_a < 2):
                 string += 'a'
                 a -= 1
                 n_a += 1
                 n_b, n_c = 0, 0
             elif (b >= a and b >= c and n_b < 2) or \
-                (n_a == 2 and b > 0 and n_b < 2) or \
-                    (n_c == 2 and b > 0 and n_b < 2):
+                 (n_a == 2 and b > 0 and n_b < 2) or \
+                 (n_c == 2 and b > 0 and n_b < 2):
                 string += 'b'
                 b -= 1
                 n_b += 1
